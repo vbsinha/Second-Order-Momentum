68bce6022c53a4bbb74bc8f0c6207a26c38728c6
Sukrut Rao <raosukrut@gmail.com>
2017-11-20 22:19:02 +0530

    Convert methods to classes

--- src/methods.py
+++ src/methods.py
@@ -1,15 +1,29 @@
 import numpy as np
 
-def first_order(function,x):
-	gradient = function.grad_f(x)
-	return gradient 
+class FirstOrder:
+	
+	def __call__(self,function,x):
+		gradient = function.grad_f(x)
+		return gradient 
 		
-def second_order(function,x):
-	grad_2 = function.grad2_f(x)
-	grad_1 = function.grad_f(x)
-	grad_inv = np.linalg.inv(grad_2)
-	update = np.matmul(grad_inv,grad_1)
-	return update 
+class SecondOrder:
+	
+	def __call__(self,function,x):
+		grad_2 = function.grad2_f(x)
+		grad_1 = function.grad_f(x)
+		grad_inv = np.linalg.inv(grad_2)
+		update = np.matmul(grad_inv,grad_1)
+		return update
+	
+class BFGS:
+	
+	def __init__(self,initial_H):
+		self.H = initial_H
+		
+	def __call__(self,function,x):
+		
+	
+
 	
 
 		
