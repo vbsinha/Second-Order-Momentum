74ba2e656a59690448e8fed6ce4b55f3c23d3210
Vaibhav Sinha <cs15btech11034@iith.ac.in>
2017-11-20 23:14:19 +0530

    Fixed BFGS

--- src/functions/examplefunc.py
+++ src/functions/examplefunc.py
@@ -1,24 +1,24 @@
 from function import Function
 import numpy as np
         
-class QuadraticFunc(Function):
-    
+class ExampleFunc(Function):
+
     def f(self,x):
-    	assert x.shape[0] == 2, "Passed point is not 2 dimensional"
-		x1 = x[0]
-		x2 = x[1]
-		obj = x1**2 - 2.0 * x1 * x2 + 4 * x2**2
-		return obj
+        assert x.shape[0] == 2, "Passed point is not 2 dimensional"
+        x1 = x[0]
+        x2 = x[1]
+        obj = x1**2 - 2.0 * x1 * x2 + 4 * x2**2
+        return obj
 
 	# define objective gradient
-	def grad_f(self,x):
-		assert x.shape[0] == 2, "Passed point is not 2 dimensional"
-		x1 = x[0]
-		x2 = x[1]
-		grad = []
-		grad.append(2.0 * x1 - 2.0 * x2)
-		grad.append(-2.0 * x1 + 8.0 * x2)
-		return grad
+    def grad_f(self,x):
+        assert x.shape[0] == 2, "Passed point is not 2 dimensional"
+        x1 = x[0]
+        x2 = x[1]
+        grad = []
+        grad.append(2.0 * x1 - 2.0 * x2)
+        grad.append(-2.0 * x1 + 8.0 * x2)
+        return np.array(grad)
         
     def grad2_f(self, x):
         assert x.shape[0] == 2, "Passed point is not 2 dimensional"
