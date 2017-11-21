e0fa0cb26a74cd5d3fed4f6b36f304bbc2c84fcb
Sukrut Rao <raosukrut@gmail.com>
2017-11-21 16:24:59 +0530

    Implement Backtracking Line Search

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from functions.ryanfunc import RyanFunc
 from functions.non_quadratic_func import NonQuadraticFunc
 from functions.examplefunc import ExampleFunc
 from gd import GradientDescent
+from step_sizes import *
 
 x = np.array([-3, 2])
 q = ExampleFunc()
@@ -14,11 +15,11 @@ g_first_mom = GradientDescent(q, methods.FirstOrder(), 'momentum', 0.9)
 # g_second = GradientDescent(q, methods.SecondOrder(), 'momentum', 0)
 # g_second_mom = GradientDescent(q, methods.SecondOrder(), 'momentum', 0.9)
 g_bfgs = GradientDescent(q, methods.BFGS(np.identity(2)), 'momentum', 0)
-points_first, x_first = g_first.gradient_descent(x, 1000, 0.01)
-points_first_mom, x_first_mom = g_first_mom.gradient_descent(x, 1000, 0.01)
-# points_second, x_second = g_second.gradient_descent(x, 1000, 0.01)
-# points_second_mom, x_second_mom = g_second_mom.gradient_descent(x, 1000, 0.01)
-points_bfgs, x_bfgs = g_bfgs.gradient_descent(x, 1000, 0.01)
+points_first, x_first = g_first.gradient_descent(x, 1000, FixedStep(0.01))
+points_first_mom, x_first_mom = g_first_mom.gradient_descent(x, 1000, FixedStep(0.01))
+# points_second, x_second = g_second.gradient_descent(x, 1000, FixedStep(0.01))
+# points_second_mom, x_second_mom = g_second_mom.gradient_descent(x, 1000, FixedStep(0.01))
+points_bfgs, x_bfgs = g_bfgs.gradient_descent(x, 1000, FixedStep(0.01))
 print "Minima achieved at (without momentum) : ", x_first
 print "Minima achieved at (with momentum) : ", x_first_mom
 # print "Minima achieved at for second order (without momentum) : ", x_second
---
+++ src/step_sizes.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class FixedStep:
+	
+	def __init__(self,eta):
+		self.eta = eta
+		
+	def __call__(self,i,x,function,method):
+		return self.eta
+		
+class BacktrackingLineStep:
+	
+	def __init__(self,alpha,beta,max_backtracks=float('inf')):
+		self.alpha = alpha
+		self.beta = beta
+		self.max_backtracks = max_backtracks
+		
+	def __call__(self,i,x,function,method):
+		U = self.method(function,x)
+		grad_f = function.grad_f(x)
+		t = 1
+		lhs = function.f(x-t*grad_f)
+		rhs = function.f(x) + alpha*t*np.dot(grad_f,-U)
+		counter = 0
+		while counter < self.max_backtracks and lhs > rhs:
+			t = beta*t
+			lhs = function.f(x-t*grad_f)
+			rhs = function.f(x) + alpha*t*np.dot(grad_f,-U)
+			counter += 1
+		return t
+		
