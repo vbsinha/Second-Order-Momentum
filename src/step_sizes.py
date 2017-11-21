d15027d6f369094987c355b399f1d1377d7c85ff
Sukrut Rao <raosukrut@gmail.com>
2017-11-21 16:34:40 +0530

    Attempt Backtracking Line Search on Quadratic

--- src/step_sizes.py
+++ src/step_sizes.py
@@ -16,16 +16,16 @@ class BacktrackingLineStep:
 		self.max_backtracks = max_backtracks
 		
 	def __call__(self,i,x,function,method):
-		U = self.method(function,x)
+		U = method(function,x)
 		grad_f = function.grad_f(x)
 		t = 1
 		lhs = function.f(x-t*grad_f)
-		rhs = function.f(x) + alpha*t*np.dot(grad_f,-U)
+		rhs = function.f(x) + self.alpha*t*np.dot(grad_f,-U)
 		counter = 0
 		while counter < self.max_backtracks and lhs > rhs:
-			t = beta*t
+			t = self.beta*t
 			lhs = function.f(x-t*grad_f)
-			rhs = function.f(x) + alpha*t*np.dot(grad_f,-U)
+			rhs = function.f(x) + self.alpha*t*np.dot(grad_f,-U)
 			counter += 1
 		return t
 		
