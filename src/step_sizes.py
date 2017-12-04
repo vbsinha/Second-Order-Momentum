a58eea53eec6d5215b10c240f4943bd5a3b569f2
Sukrut Rao <raosukrut@gmail.com>
2017-12-04 22:28:00 +0530

    Ensure BTLS uses momentum update

--- src/step_sizes.py
+++ src/step_sizes.py
@@ -5,8 +5,26 @@ class FixedStep:
 	def __init__(self,eta):
 		self.eta = eta
 		
-	def __call__(self,i,x,function,method):
+	def __call__(self,i,x,U,function):
 		return self.eta
+		
+class DiminishingStep:
+	
+	def __init__(self,eta):
+		self.eta = eta
+		
+	def __call__(self,i,x,U,function):
+		return float(self.eta)/float(i)
+		
+class RangeDiminishingStep:
+	
+	def __init__(self,initial,final,N):
+		self.initial = initial
+		self.final = final
+		self.N = N
+		
+	def __call__(self,i,x,U,function):
+		return initial + float((final-initial)/float(N))*float(i)
 		
 class BacktrackingLineStep:
 	
@@ -15,8 +33,8 @@ class BacktrackingLineStep:
 		self.beta = beta
 		self.max_backtracks = max_backtracks
 		
-	def __call__(self,i,x,function,method):
-		U = method(function,x)
+	def __call__(self,i,x,U,function):
+#		U = method(function,x)
 		grad_f = function.grad_f(x)
 		t = 1
 		lhs = function.f(x-t*U)
