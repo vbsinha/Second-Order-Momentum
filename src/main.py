f3a5bccdbd6b43ddccc92a6761e933265ee17cc3
Vaibhav Sinha <cs15btech11034@iith.ac.in>
2017-12-05 11:35:20 +0530

    Few additions in tex

--- src/main.py
+++ src/main.py
@@ -17,29 +17,29 @@ from step_sizes import *
 
 epsilon = 1e-15
 
-q = RyanFunc()
+q = CubicRegFunc()
 x = q.start_x()  
 g_first = GradientDescent(q, methods.FirstOrder())
-g_first_mom = GradientDescent(q, methods.FirstOrder(), 'momentum', 0.9)
+g_first_mom = GradientDescent(q, methods.FirstOrder(), 'momentum', 0.1)
 g_second = GradientDescent(q, methods.SecondOrder())
-g_second_mom = GradientDescent(q, methods.SecondOrder(), 'momentum', 0.9)
+g_second_mom = GradientDescent(q, methods.SecondOrder(), 'momentum', 0.1)
 g_bfgs = GradientDescent(q, methods.BFGS(np.identity(2)))
-g_bfgs_mom = GradientDescent(q, methods.BFGS(np.identity(2)), 'momentum', 0.9)
-# g_cr = GradientDescent(q, methods.CubicRegularization())
-print x
-points_first, x_first = g_first.gradient_descent(x, 500, FixedStep(0.01))
-points_first_mom, x_first_mom = g_first_mom.gradient_descent(x, 500, FixedStep(0.01))
-points_second, x_second = g_second.gradient_descent(x, 500, FixedStep(0.1))
-points_second_mom, x_second_mom = g_second_mom.gradient_descent(x, 500, FixedStep(0.1))
-points_bfgs, x_bfgs = g_bfgs.gradient_descent(x, 500, FixedStep(0.1))
-points_bfgs_mom, x_bfgs_mom = g_bfgs_mom.gradient_descent(x, 500, FixedStep(0.1)) 
-# points_cr, x_cr = g_cr.gradient_descent(x, 100, FixedStep(-1))
+g_bfgs_mom = GradientDescent(q, methods.BFGS(np.identity(2)), 'momentum', 0.1)
+g_cr = GradientDescent(q, methods.CubicRegularization())
+# print x
+points_first, x_first = g_first.gradient_descent(x, 80, FixedStep(0.1))
+points_first_mom, x_first_mom = g_first_mom.gradient_descent(x, 80, FixedStep(0.1))
+points_second, x_second = g_second.gradient_descent(x, 80, FixedStep(1))
+points_second_mom, x_second_mom = g_second_mom.gradient_descent(x, 80, FixedStep(1))
+points_bfgs, x_bfgs = g_bfgs.gradient_descent(x, 80, FixedStep(0.1))
+points_bfgs_mom, x_bfgs_mom = g_bfgs_mom.gradient_descent(x, 80, FixedStep(0.1))
+points_cr, x_cr = g_cr.gradient_descent(x, 100, FixedStep(-1))
 print "Minima achieved at (without momentum) : ", x_first
 print "Minima achieved at (with momentum) : ", x_first_mom
 print "Minima achieved at for second order (without momentum) : ", x_second
 print "Minima achieved at for second order (with momentum) : ", x_second_mom
 print "Minima achieved at for bfgs (without momentum) : ", x_bfgs
 print "Minima achieved at for bfgs (with momentum) : ", x_bfgs_mom
-# print "Minima achieved at for Cubic Regularization (without momentum) : ", x_cr
-plot(q, q.domain()[0], q.domain()[1], [(points_first, "First order"), (points_first_mom, "First order with momentum"), (points_bfgs, "BFGS without momentum"), (points_bfgs_mom, "BFGS with momentum"),(points_second, "Second order"), (points_second_mom, "Second order with momentum")]) #, (points_cr, "Cubic Regularization") ])
-plot_convergence_rate(q, [0,500], [(points_first, "First order"), (points_first_mom, "First order with momentum"), (points_bfgs, "BFGS without momentum"), (points_bfgs_mom, "BFGS with momentum"),(points_second, "Second order"), (points_second_mom, "Second order with momentum")])#, (points_cr, "Cubic Regularization")])
+print "Minima achieved at for Cubic Regularization (without momentum) : ", x_cr
+plot(q, q.domain()[0], q.domain()[1], [(points_first, "First order"), (points_first_mom, "First order with momentum"), (points_bfgs, "BFGS without momentum"), (points_bfgs_mom, "BFGS with momentum"),(points_second, "Second order"), (points_second_mom, "Second order with momentum"), (points_cr, "Cubic Regularization") ])
+plot_convergence_rate(q, [0,500], [(points_first, "First order"), (points_first_mom, "First order with momentum"), (points_bfgs, "BFGS without momentum"), (points_bfgs_mom, "BFGS with momentum"),(points_second, "Second order"), (points_second_mom, "Second order with momentum"), (points_cr, "Cubic Regularization")])
